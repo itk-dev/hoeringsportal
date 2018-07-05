02a32438d5577f4add4492bd8903b98cc77ac3cf
Mikkel Ricky <rimi@aarhus.dk>
2018-07-05 11:22:12 +0200

    HRIN-28: Applied coding standards

--- web/themes/custom/hoeringsportal/assets/js/hide_password_help.js
+++ web/themes/custom/hoeringsportal/assets/js/hide_password_help.js
@@ -1,12 +1,12 @@
-$ = require('jquery')
+const $ = require('jquery')
 
 // Function for hiding password confirmation text.
 var passwordConfirm = () => {
   if (!$('input.js-password-field').val()) {
-    $('div.js-password-confirm').css("visibility", "hidden");
+    $('div.js-password-confirm').css('visibility', 'hidden')
   }
 }
 
 $(() => {
-  passwordConfirm();
+  passwordConfirm()
 })
