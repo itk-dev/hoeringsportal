0d051554b1762e0bc7b839793348ced013477fba
Martin Yde Granath <my@aarhus.dk>
2018-08-20 09:43:35 +0200

    HRIN-88: Added timeline

---
+++ web/modules/custom/hoeringsportal_project_timeline/assets/js/project-timeline.js
@@ -0,0 +1,3 @@
+require('../node_modules/vis/dist/vis.min.css')
+require('../node_modules/vis/dist/vis.min.js')
+require('./hoeringsportal_timeline.js')
---
+++ web/modules/custom/hoeringsportal_project_timeline/src/Plugin/Block/ProjectTimeline.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\hoeringsportal_project_timeline\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides timeline content.
+ *
+ * @Block(
+ *   id = "project_timeline",
+ *   admin_label = @Translation("Project timeline"),
+ * )
+ */
+class ProjectTimeline extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $a = 1;
+    return [
+      '#theme' => 'hoeringsportal_project_timeline',
+      '#node' => $node,
+      '#attached' => [
+
+        'library' => [
+          'hoeringsportal_project_timeline/project_timeline',
+        ],
+      ],
+    ];
+  }
+
+}
