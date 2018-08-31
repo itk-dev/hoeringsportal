b5a6715385a547e90779d3fc8836cdca34ec7a4b
Martin Yde Granath <my@aarhus.dk>
2018-08-31 11:17:19 +0200

    HRIN-127

--- web/modules/custom/hoeringsportal_project_timeline/assets/js/hoeringsportal_timeline.js
+++ web/modules/custom/hoeringsportal_project_timeline/assets/js/hoeringsportal_timeline.js
@@ -1,21 +1,60 @@
 import vis from 'vis/dist/vis.js'
 
-// DOM element where the Timeline will be attached
-var container = document.getElementById('visualization')
+Drupal.behaviors.timeline = {
+  attach: function (context, settings) {
+    console.log(settings);
+    // DOM element where the Timeline will be attached
+    var container = document.getElementById('visualization')
 
-// Create a DataSet (allows two way data-binding)
-var items = new vis.DataSet([
-  {id: 1, content: 'item 1', start: '2014-04-20'},
-  {id: 2, content: 'item 2', start: '2014-04-14'},
-  {id: 3, content: 'item 3', start: '2014-04-18'},
-  {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-  {id: 5, content: 'item 5', start: '2014-04-25'},
-  {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-])
+    // Create a DataSet (allows two way data-binding)
+    var items = new vis.DataSet([
+      {
+        id: 1,
+        className: 'abc',
+        content: '<div data-container="body" data-toggle="popover" title="Popover title" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">item 1</div>',
+        start: '2018-04-20'
+      },
+      {id: 2, content: 'item 2', start: '2018-01-14'},
+      {id: 3, content: 'item 3', start: '2018-04-18'},
+      {id: 4, content: 'item 4', start: '2018-04-16', end: '2018-04-19'},
+      {id: 5, content: 'item 5', start: '2018-04-25'},
+      {id: 6, content: 'item 6', start: '2018-04-27', type: 'point'}
+    ])
 
-// Configuration for the Timeline
-var options = {}
+    var newItems = new vis.DataSet({});
+    for (var i = 0; i < settings.timelineItems.length; i++) {
+      var item = settings.timelineItems[i];
+      console.log(item);
+      newItems.add({
+        id: i,
+        className: item.type + ' ' + item.state,
+        content: '<div class="timeline-item-inner" data-container="timeline-item-id-' + i + '" data-toggle="popover" title="' + item.title + '" data-placement="top" data-content="' + item.startDate + ' - ' + item.endDate + '">' + item.title + '</div>',
+        start: item.startDate
+      });
+    }
 
-// Create a Timeline
-var timeline = new vis.Timeline(container, items, options)
-console.log(timeline)
+    var now = Date.now()
+
+    // Configuration for the Timeline
+    var options = {
+      zoomable: false,
+      start: now - 31540000000,
+      end: now + 31540000000
+    }
+
+    // Create a Timeline
+    var timeline = new vis.Timeline(container, newItems, options)
+
+    timeline.on('click', function (properties) {
+      if(properties.what == 'item') {
+        $(function () {
+          $('.timeline-item-inner').popover({
+            container: 'timeline-item-id-' + properties.item
+          })
+        })
+        console.log('asdf');
+      }
+      console.log(properties);
+    });
+  }
+}
--- web/modules/custom/hoeringsportal_project_timeline/src/Plugin/Block/ProjectTimeline.php
+++ web/modules/custom/hoeringsportal_project_timeline/src/Plugin/Block/ProjectTimeline.php
@@ -20,13 +20,63 @@ class ProjectTimeline extends BlockBase {
   public function build() {
     $node = \Drupal::routeMatch()->getParameter('node');
     $a = 1;
+
+    /*
+    Example array of a timelineItem
+
+    array(
+    'title' => 'testing title',
+    'startDate' => '2018-06-21',
+    'endDate' => '2018-08-05',
+    'type' => 'hearing',
+    'state' => 'passed'
+    ),
+     */
+    $timeline_items = [
+      [
+        'title' => 'Project start',
+        'startDate' => $node->field_project_start->value,
+        'endDate' => $node->field_project_finish->value,
+        'type' => 'system',
+        'state' => 'passed',
+      ],
+      [
+        'title' => t('Project finish'),
+        'startDate' => $node->field_project_finish->value,
+        'endDate' => $node->field_project_finish->value,
+        'type' => 'system',
+        'state' => 'passed',
+      ],
+    ];
+
+    $timeline_items_static = [
+      [
+        'title' => 'testing title',
+        'startDate' => '2018-06-21',
+        'endDate' => '2018-08-05',
+        'type' => 'hearing',
+        'state' => 'passed',
+      ],
+      [
+        'title' => 'testing extra',
+        'startDate' => '2019-06-21',
+        'endDate' => '2019-08-05',
+        'type' => 'hearing',
+        'state' => 'upcomming',
+      ],
+    ];
+
+    $timeline_items = array_merge($timeline_items, $timeline_items_static);
+
     return [
       '#theme' => 'hoeringsportal_project_timeline',
       '#node' => $node,
       '#attached' => [
-
         'library' => [
           'hoeringsportal_project_timeline/project_timeline',
+        ],
+        'drupalSettings' => [
+          'timelineItems' => $timeline_items,
         ],
       ],
     ];
