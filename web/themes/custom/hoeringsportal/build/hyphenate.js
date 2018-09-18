9be43b0d23c968b45e1d91d167d16dd03fa75e94
Martin Yde Granath <my@aarhus.dk>
2018-09-18 15:11:51 +0200

    HRIN-161: Added hyphenation

---
+++ web/themes/custom/hoeringsportal/build/hyphenate.js
@@ -0,0 +1,14 @@
+var Hyphenopoly = {
+  require: {
+    "da": "Add hyphenation"
+  },
+  paths: {
+    patterndir: "./themes/custom/hoeringsportal/assets/Hyphenopoly/",
+    maindir: "./"
+  },
+  setup: {
+    classnames: {
+      "hyphenate": {}
+    }
+  }
+};
