2acf260ae4bdc6059efa218b38afacf4d88c00f2
Martin Yde Granath <my@aarhus.dk>
2020-05-06 12:35:49 +0200

    HRIN-524: Added block for signup in sidebar

---
+++ web/modules/custom/hoeringsportal_public_meeting/src/Plugin/Block/PublicMeetingSummaryBlock.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\hoeringsportal_public_meeting\Plugin\Block;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\hoeringsportal_public_meeting\Helper\PublicMeetingHelper;
+
+/**
+ * Provides a Public meeting summary Block.
+ *
+ * @Block(
+ *   id = "hoeringsportal_public_meeting_summary_block",
+ *   admin_label = @Translation("Public meeting summary"),
+ *   category = @Translation("Public meeting"),
+ * )
+ */
+class PublicMeetingSummaryBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Helper class for public meetings.
+   *
+   * @var \Drupal\hoeringsportal_public_meeting\Helper\PublicMeetingHelper
+   */
+  protected $helper;
+
+  /**
+   * Block constructor.
+   *
+   * @param array $configuration
+   *   Block configuration.
+   * @param string $plugin_id
+   *   The plugin id.
+   * @param mixed $plugin_definition
+   *   Definition of plugin.
+   * @param \Drupal\hoeringsportal_public_meeting\Helper\PublicMeetingHelper $helper
+   *   Helper class.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PublicMeetingHelper $helper) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->helper = $helper;
+  }
+
+  /**
+   * Create function for block.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The service container.
+   * @param array $configuration
+   *   Block configuration.
+   * @param string $plugin_id
+   *   The plugin id.
+   * @param mixed $plugin_definition
+   *   Definition of plugin.
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('hoeringsportal_public_meeting.public_meeting_helper')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    if (!$this->helper->isPublicMeeting($node)) {
+      throw new NotFoundHttpException();
+    }
+
+    $cacheTags = $node->getCacheTags();
+
+    $cacheContexts = $node->getCacheContexts();
+    $cacheContexts[] = 'url';
+    $cacheContexts = array_unique($cacheContexts);
+
+    return [
+      '#theme' => 'hoeringsportal_public_meeting_summary',
+      '#pretix_signup' => $node->field_pretix_dates[0]->getValue(),
+      '#signup_deadline' => $node->field_registration_deadline[0]->getValue(),
+      '#node' => $node,
+      '#cache' => [
+        'contexts' => $cacheContexts,
+        'tags' => $cacheTags,
+      ],
+    ];
+  }
+
+}
