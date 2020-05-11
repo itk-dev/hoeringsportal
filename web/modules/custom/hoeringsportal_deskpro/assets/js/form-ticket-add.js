d4acdc5d407a60f30e1427ae0ec0d51e01a6a798
Mikkel Ricky <rimi@aarhus.dk>
2020-05-11 10:45:19 +0200

    HRIN-510: Cleaned up after code review

--- web/modules/custom/hoeringsportal_deskpro/assets/js/form-ticket-add.js
+++ web/modules/custom/hoeringsportal_deskpro/assets/js/form-ticket-add.js
@@ -1,6 +1,7 @@
 require('../css/form-ticket-add.css')
 
 const $ = require('jquery')
+require('jquery-validation')
 require('jquery-ui/ui/widgets/autocomplete')
 
 // @see https://stackoverflow.com/a/11845718
@@ -146,9 +147,10 @@ $(() => {
       // Set labels
       const labels = {
         // Field name => label
-        'name': isPersonal ? 'Dit navn' : `Navn (${text})`,
-        'email': isPersonal ? 'Din e-mailadresse' : `E-mailadresse (${text})`,
-        'address': isPersonal ? 'Din adresse' : `Adresse (${text})`,
+        'name': isPersonal ? Drupal.t('Your name') : Drupal.t('Name (@organization)', {'@organization': text}),
+        'email': isPersonal ? Drupal.t('Your email address') : Drupal.t('Email address (@organization)', {'@organization': text}),
+        'postal-code-and-city': isPersonal ? Drupal.t('Your postal code and city') : Drupal.t('Postal code and city (@organization)', {'@organization': text}),
+        'street-and-number': isPersonal ? Drupal.t('Your street and number') : Drupal.t('Street and number (@organization)', {'@organization': text}),
       }
       for (let [key, label] of Object.entries(labels)) {
         const selector = `[for="edit-${key}"]`
