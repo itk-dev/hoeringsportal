6d652093d748f65edb0c4a8970be55e612cb5a61
Martin Yde Granath <my@aarhus.dk>
2020-05-14 12:23:03 +0200

    HRIN-515: Changed html element

--- web/modules/custom/hoeringsportal_project_timeline/assets/js/project-timeline-backend.js
+++ web/modules/custom/hoeringsportal_project_timeline/assets/js/project-timeline-backend.js
@@ -6,6 +6,6 @@
 // Move help text of timeline paragraph.
 let helpText = document.getElementById('edit-field-timeline-items--description')
 let timelineWrapper = document.getElementById('edit-field-project-start-wrapper')
-let helpTextWrapper = document.createElement('p')
+let helpTextWrapper = document.createElement('div')
 timelineWrapper.prepend(helpTextWrapper)
 helpTextWrapper.appendChild(helpText)
