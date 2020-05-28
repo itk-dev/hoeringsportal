8f5adc1c8e59b5248b31222719654fa069999374
Mikkel Ricky <rimi@aarhus.dk>
2020-05-28 08:56:16 +0200

    HRIN-510: Cleaned up JavaScript

--- web/modules/custom/hoeringsportal_deskpro/assets/js/form-ticket-add.js
+++ web/modules/custom/hoeringsportal_deskpro/assets/js/form-ticket-add.js
@@ -1,3 +1,4 @@
+/* global Drupal */
 require('../css/form-ticket-add.css')
 
 const $ = require('jquery')
@@ -139,24 +140,28 @@ $(() => {
     }
   })
 
-  $('[data-drupal-selector="edit-representation"]').on('change', function () {
-    const value = $(this).val()
-    const isPersonal = value == 5
-    const text = $('option:selected', this).text()
+  const updateUI = () => {
+    const $representation = $('[data-drupal-selector="edit-representation"]')
+    const value = $representation.val()
+    const isPersonal = parseInt(value) === 5
+    const text = $representation.find('option:selected').text()
+
     if (text) {
       // Set labels
       const labels = {
         // Field name => label
-        'name': isPersonal ? Drupal.t('Your name') : Drupal.t('Name (@organization)', {'@organization': text}),
-        'email': isPersonal ? Drupal.t('Your email address') : Drupal.t('Email address (@organization)', {'@organization': text}),
-        'postal-code-and-city': isPersonal ? Drupal.t('Your postal code and city') : Drupal.t('Postal code and city (@organization)', {'@organization': text}),
-        'street-and-number': isPersonal ? Drupal.t('Your street and number') : Drupal.t('Street and number (@organization)', {'@organization': text}),
+        'name': isPersonal ? Drupal.t('Your name') : Drupal.t('Name (@organization)', { '@organization': text }),
+        'email': isPersonal ? Drupal.t('Your email address') : Drupal.t('Email address (@organization)', { '@organization': text }),
+        'postal-code-and-city': isPersonal ? Drupal.t('Your postal code and city') : Drupal.t('Postal code and city (@organization)', { '@organization': text }),
+        'street-and-number': isPersonal ? Drupal.t('Your street and number') : Drupal.t('Street and number (@organization)', { '@organization': text })
       }
       for (let [key, label] of Object.entries(labels)) {
         const selector = `[for="edit-${key}"]`
-        console.log(selector, $(selector))
         $(selector).text(label)
       }
     }
-  })
+  }
+
+  $('[data-drupal-selector="edit-representation"]').on('change', updateUI)
+  updateUI()
 })
