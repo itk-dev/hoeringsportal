591a3f965f4bb4194bb3c0b5d6a4ad1d0e3708cf
Mikkel Ricky <rimi@aarhus.dk>
2020-10-23 13:17:54 +0200

    HRIN-599: Hid date part of meeting end time

--- web/modules/custom/hoeringsportal_public_meeting/assets/js/form.js
+++ web/modules/custom/hoeringsportal_public_meeting/assets/js/form.js
@@ -16,5 +16,19 @@
       $checkboxes.find('input[type="checkbox"]').prop('checked', false)
     })
     $buttons.append($selectNone)
+
+    // Copy date from meeting start time to meeting end time.
+    var $meetingStartTimes = $('[name^="field_last_meeting_time"][name$="[value][date]"')
+    $meetingStartTimes.on('change', function () {
+      var match = /\[(\d+)\]/.exec($(this).attr('name'))
+      if (match) {
+        var index = parseInt(match[1])
+        var endTime = $('[name="field_last_meeting_time_end['+index+'][value][date]"')
+        $(endTime).val($(this).val())
+      }
+    })
+    $meetingStartTimes.trigger('change')
+    // Hide date part of the meeting end time.
+    $('[name^="field_last_meeting_time_end"][name$="[value][date]"').hide()
   })
 }(jQuery));
