506fc6521430ee8cba9db2cac7a703d510b83c0a
Mikkel Ricky <rimi@aarhus.dk>
2021-10-01 09:48:31 +0200

    SUPP0RT-306: Removed Select all/none buttons. Emphasized "Hele kommunen".

--- web/modules/custom/hoeringsportal_hearing/assets/js/form.js
+++ web/modules/custom/hoeringsportal_hearing/assets/js/form.js
@@ -1,20 +1,10 @@
 (function ($) {
   $(function() {
-    // Add "Select all" and "Select none" button for "Area".
-    $checkboxes = $('.field--type-entity-reference.field--name-field-area .fieldset-wrapper .form-checkboxes')
-    $buttons = $('<div class="buttons"/>')
-    $checkboxes.before($buttons)
-
-    $selectAll = $('<button type="button" class="button button--small">' + Drupal.t('Select all') + '</button>')
-    $selectAll.on('click', function() {
-      $checkboxes.find('input[type="checkbox"]').prop('checked', true)
+    // Emphasize "Hele kommunen" in "Area" selection.
+    $('.field--type-entity-reference.field--name-field-area .fieldset-wrapper .form-checkboxes label').filter(function () {
+      if ('Hele kommunen' === $(this).text()) {
+        $(this).wrapInner('<strong/>')
+      }
     })
-    $buttons.append($selectAll)
-
-    $selectNone = $('<button type="button" class="button button--small">' + Drupal.t('Select none') + '</button>')
-    $selectNone.on('click', function() {
-      $checkboxes.find('input[type="checkbox"]').prop('checked', false)
-    })
-    $buttons.append($selectNone)
   })
 }(jQuery));
