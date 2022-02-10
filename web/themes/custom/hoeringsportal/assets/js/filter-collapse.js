41f80ac4350360077dd4029742dea4761d5b3faa
Jesper Pedersen <pejes@aarhus.dk>
2022-02-10 16:32:11 +0100

    HRIN-582: Add collapse on desktop. show categories as grid.

--- web/themes/custom/hoeringsportal/assets/js/filter-collapse.js
+++ web/themes/custom/hoeringsportal/assets/js/filter-collapse.js
@@ -5,19 +5,12 @@
 
 /* eslint-env jquery */
 (function ($) {
-  let width = $(window).width()
-  let params = window.location.search
-  let collapseFilterId = '#collapseFilters'
-  $(document).ready(function () {
-    if (width > 768 || params.length > 0) {
-      $('#collapseFilters').collapse()
-    }
-  })
+  let collapseFilterId = "#collapseFilters";
 
-  $(collapseFilterId).on('show.bs.collapse', function () {
-    $('.filters-toggle span').text('Skjul filtre')
-  })
-  $(collapseFilterId).on('hide.bs.collapse', function () {
-    $('.filters-toggle span').text('Vis filtre')
-  })
-})(jQuery)
+  $(collapseFilterId).on("show.bs.collapse", function () {
+    $(".filters-toggle span").text("Skjul filtre");
+  });
+  $(collapseFilterId).on("hide.bs.collapse", function () {
+    $(".filters-toggle span").text("Vis filtre");
+  });
+})(jQuery);
