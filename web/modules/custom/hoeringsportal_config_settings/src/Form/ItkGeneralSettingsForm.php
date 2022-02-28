085a3a8ff45410ea0999f39c2f2976ae82bb0c01
martinyde <yde001@gmail.com>
2022-02-28 12:40:51 +0100

    Fixed add messages

--- web/modules/custom/hoeringsportal_config_settings/src/Form/ItkGeneralSettingsForm.php
+++ web/modules/custom/hoeringsportal_config_settings/src/Form/ItkGeneralSettingsForm.php
@@ -200,7 +200,7 @@ class ItkGeneralSettingsForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal\Core\Messenger\MessengerInterface::addMessage('Settings saved');
+    \Drupal::messenger()->addMessage('Settings saved');
 
     // Set the rest of the configuration values.
     $this->getBaseConfig()->setMultiple([
