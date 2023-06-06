7975fdf2220969ff7b11d8ea9e22499d018989ae
martinyde <yde001@gmail.com>
2023-06-06 14:43:01 +0200

    Added social share buttons and meta tags

--- web/modules/custom/hoeringsportal_citizen_proposal/src/Helper/Helper.php
+++ web/modules/custom/hoeringsportal_citizen_proposal/src/Helper/Helper.php
@@ -10,6 +10,7 @@ use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Serializer\Serializer;
+use Drupal\Core\File\FileUrlGenerator;
 use Drupal\Core\Messenger\MessengerTrait;
 
 /**
@@ -28,6 +29,7 @@ class Helper {
     readonly private PrivateTempStoreFactory $tempStoreFactory,
     readonly private Serializer $serializer,
     readonly private State $state,
+    readonly private FileUrlGenerator $fileUrlGenerator,
   ) {
   }
 
@@ -85,6 +87,56 @@ class Helper {
    */
   public function preprocessForm(&$variables): void {
     $variables['admin_form_state_values'] = $this->state->get('citizen_proposal_admin_form_values');
+  }
+
+  /**
+   * Page attachments related to citizen proposal.
+   */
+  public function proposalPageAttachments(&$page): void {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if (!$node) {
+      return;
+    }
+
+    if ('citizen_proposal' !== $node->bundle()) {
+      return;
+    }
+
+    $og = [
+      'title' => [
+        '#tag' => 'meta',
+        '#attributes' => [
+          'property' => 'og:title',
+          'content' => $node->getTitle(),
+        ],
+      ],
+      'image' => [
+        '#tag' => 'meta',
+        '#attributes' => [
+          'property' => 'og:image',
+          'content' => $this->fileUrlGenerator->generateAbsoluteString(\Drupal::config('hoeringsportal.settings')->get('logo.path')),
+        ],
+      ],
+      'description' => [
+        '#tag' => 'meta',
+        '#attributes' => [
+          'property' => 'og:description',
+          'content' => substr($node->field_proposal->value, 0, 150) . '...',
+        ],
+      ],
+      'url' => [
+        '#tag' => 'meta',
+        '#attributes' => [
+          'property' => 'og:url',
+          'content' => Url::fromRoute('<current>', [], ['absolute' => 'true'])->toString(),
+        ],
+      ],
+    ];
+
+    foreach ($og as $key => $attr) {
+      $page['#attached']['html_head'][] = [$attr, $key];
+    }
   }
 
   /**
