806f88102c2145fd190362777dc3a1b6af56c114
Mikkel Ricky <rimi@aarhus.dk>
2023-06-06 13:53:45 +0200

    FORSLAG-35: Added Cypress tests

---
+++ cypress/e2e/citizen-proposal.cy.js
@@ -0,0 +1,81 @@
+/* global describe it cy before */
+describe('Citizen proposal', () => {
+  it('Create proposal', () => {
+    cy.viewport(1000, 1000)
+
+    cy.visit('/citizen_proposal/add')
+
+    cy.getByLabel('E-mail')
+      .type('borger87@eksemple.dk')
+
+    cy.getByLabel('Overskrift')
+      .type('Sådan løser vi alle problemer!')
+
+    cy.getByLabel('Proposal')
+      .type('Jeg synes det er et stort problem at …')
+
+    cy.getByLabel('Remarks')
+      .type('Dette vil også løse problemet med at …')
+
+    cy.get('.button').contains('Create proposal')
+      .click()
+
+    cy.url()
+      .should('include', '/citizen_proposal/approve')
+
+    cy.get('input[type="submit"]').contains('Approve proposal')
+    cy.get('a').contains('Edit proposal')
+    cy.get('input[type="submit"]').contains('Cancel proposal')
+
+    cy
+      .contains('borger87@eksemple.dk')
+
+    cy.get('a').contains('Edit proposal')
+      .click()
+
+    cy.getByLabel('E-mail')
+      .should('have.value', 'borger87@eksemple.dk')
+      .clear()
+      .type('borger87@eksempel.dk')
+
+    cy.get('.button').contains('Update proposal')
+      .click()
+
+    cy
+      .contains('borger87@eksempel.dk')
+
+    cy.get('input[type="submit"]').contains('Approve proposal')
+      .click()
+
+    cy.contains('Thank you for you submission.')
+  })
+
+  it('Cancel proposal', () => {
+    cy.viewport(1000, 1000)
+
+    cy.visit('/citizen_proposal/add')
+
+    cy.getByLabel('E-mail')
+      .type('borger87@eksemple.dk')
+
+    cy.getByLabel('Overskrift')
+      .type('Sådan løser vi alle problemer!')
+
+    cy.getByLabel('Proposal')
+      .type('Jeg synes det er et stort problem at …')
+
+    cy.getByLabel('Remarks')
+      .type('Dette vil også løse problemet med at …')
+
+    cy.get('.button').contains('Create proposal')
+      .click()
+
+    cy.url()
+      .should('include', '/citizen_proposal/approve')
+
+    cy.get('input[type="submit"]').contains('Cancel proposal')
+      .click()
+
+    cy.contains('Your submission has been cancelled.');
+  })
+})
