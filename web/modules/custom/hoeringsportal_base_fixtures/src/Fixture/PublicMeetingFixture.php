70b262fdeb4418d9013837b6502b90be665b8750
Martin Kristainsen <martinkristainsen@Martins-MacBook-Pro.local>
2023-10-11 10:38:21 +0200

    HRN-756 updated Fixture

--- web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/PublicMeetingFixture.php
+++ web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/PublicMeetingFixture.php
@@ -3,6 +3,7 @@
 namespace Drupal\hoeringsportal_base_fixtures\Fixture;
 
 use Drupal\content_fixtures\Fixture\AbstractFixture;
+use Drupal\content_fixtures\Fixture\DependentFixtureInterface;
 use Drupal\content_fixtures\Fixture\FixtureGroupInterface;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
@@ -12,7 +13,7 @@ use Drupal\node\NodeInterface;
  *
  * @package Drupal\hoeringsportal_base_fixtures\Fixture
  */
-class PublicMeetingFixture extends AbstractFixture implements FixtureGroupInterface {
+class PublicMeetingFixture extends AbstractFixture implements DependentFixtureInterface, FixtureGroupInterface {
 
   /**
    * {@inheritdoc}
@@ -35,15 +36,20 @@ class PublicMeetingFixture extends AbstractFixture implements FixtureGroupInterf
       "field_last_meeting_time" => date("Y-m-d", 1283166912) ,
       "field_last_meeting_time_end" => date("Y-m-d", 2222222322222) ,
 //      "field_map" => 'field_map' ,
-////      "field_media_document" => 'field_media_document'  ,
-////      "field_media_image_single" => 'field_media_image_single' ,
+      "field_media_document"  => ['target_id' => $this->getReference('media_library:Fil:MTM')->id()],
+      "field_media_image_single" => ['target_id' => $this->getReference('media_library:Billede:MTM')->id()] ,
 ////      "field_pretix_dates" => 'field_pretix_dates' ,
-"field_pretix_event_settings" => ['template_event' => 'testvej 1', 'synchronize_event' => FALSE],
+      "field_pretix_event_settings" => ['template_event' => 'testvej 1', 'synchronize_event' => FALSE],
       "field_email" => 'parent@test.dk ',
-////      "field_project_reference" => 'field_project_reference' ,
-//      "field_public_meeting_cancelled" => 'field_public_meeting_cancelled',
-//      "field_registration_deadline" => 'field_registration_deadline',
-////      "field_section" => 'field_section' ,
+      "field_project_reference" => 'field_project_reference' ,
+
+      //      "field_public_meeting_cancelled" => 'field_public_meeting_cancelled', Warning: file_get_contents(themes/custom/hoeringsportal/build/hoeringsportal.js)
+      "field_registration_deadline" => ['value' => 'word' ] ,
+      'field_section' =>
+        [
+          'target_id' => $this->getReference('paragraph:content_list:content_list1')->id(),
+          'target_revision_id' => $this->getReference('paragraph:content_list:content_list1')->getRevisionId(),
+        ],
 //      "field_signup_link" => 'field_signup_link' ,
 //      "field_signup_selection" => 'field_signup_selection' ,
 //      "field_signup_text" => 'field_signup_text'  ,
@@ -52,6 +58,16 @@ class PublicMeetingFixture extends AbstractFixture implements FixtureGroupInterf
     ]);
     $this->addReference('public_meeting:fixture-1', $node);
     $node->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies() {
+    return [
+      MediaFixture::class,
+      ParagraphFixture::class,
+    ];
   }
 
   /**
