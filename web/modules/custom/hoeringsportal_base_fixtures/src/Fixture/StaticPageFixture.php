e051f2c2f97cfcf825fbdd50a19d1e278dc0c650
Martin Kristainsen <martinkristainsen@Martins-MacBook-Pro.local>
2023-10-16 15:18:58 +0200

    HRN-756 added StaticPageFixture

---
+++ web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/StaticPageFixture.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\hoeringsportal_base_fixtures\Fixture;
+
+use Drupal\content_fixtures\Fixture\AbstractFixture;
+use Drupal\content_fixtures\Fixture\DependentFixtureInterface;
+use Drupal\content_fixtures\Fixture\FixtureGroupInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+
+/**
+ * Page fixture.
+ *
+ * @package Drupal\hoeringsportal_base_fixtures\Fixture
+ */
+class StaticPageFixture  extends AbstractFixture implements DependentFixtureInterface, FixtureGroupInterface {
+
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load() {
+    $node = Node::create([
+      'type' => 'static_page',
+      'title' => 'Statisk side 223232323',
+      'status' => NodeInterface::PUBLISHED,
+    ]);
+    $this->addReference('static_page:fixture-1', $node);
+    $node->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies() {
+    return [
+      MediaFixture::class,
+      ParagraphFixture::class,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGroups() {
+    return ['nodes'];
+  }
+
+}
