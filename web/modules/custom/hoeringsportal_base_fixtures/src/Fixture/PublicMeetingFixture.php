71ed5639757ff895c068b273c2543ccc6840e2d9
Mikkel Ricky <rimi@aarhus.dk>
2024-01-17 15:27:12 +0100

    125: Added department on public meetings

--- web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/PublicMeetingFixture.php
+++ web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/PublicMeetingFixture.php
@@ -50,6 +50,9 @@ class PublicMeetingFixture extends AbstractFixture implements DependentFixtureIn
       "field_signup_selection" => 'field_signup_selection' ,
       "field_signup_text" => 'field_signup_text'  ,
       "field_teaser" => 'field_teaser' ,
+      'field_department' => [
+        $this->getReference('department:Department 2')->id(),
+      ],
     ]);
     $this->addReference('public_meeting:fixture-1', $node);
     $node->save();
@@ -62,6 +65,7 @@ class PublicMeetingFixture extends AbstractFixture implements DependentFixtureIn
     return [
       MediaFixture::class,
       ParagraphFixture::class,
+      TermDepartmentFixture::class,
     ];
   }
 
