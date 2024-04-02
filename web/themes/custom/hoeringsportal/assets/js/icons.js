a68445a4720ae4a0aedca84a2b43e2899b223feb
Jesper Pedersen <jesper@yepz.dk>
2024-04-02 10:57:04 +0200

    1029: Replace better social sharings buttens sprite with fontawesome icons. Clean up the use of fontawesome icons.

--- web/themes/custom/hoeringsportal/assets/js/icons.js
+++ web/themes/custom/hoeringsportal/assets/js/icons.js
@@ -1,7 +1,52 @@
-// Add fontawesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSort } from '@fortawesome/free-solid-svg-icons/faSort'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
+/*
+// Add fontawesome icons
+*/
 
-library.add(faSort)
-library.add(faEnvelope)
+// Import the svg core
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// To keep the package size as small as possible we only import icons we use
+
+// Import the icons from the free solid package.
+import {
+  faSort,
+  faEnvelope,
+  faSearch,
+  faTimes,
+  faCopy,
+  faPrint
+} from '@fortawesome/free-solid-svg-icons'
+
+// Import icons from the free brands package
+import {
+  faFacebook,
+  faXTwitter,
+  faLinkedin,
+  faWhatsapp,
+  faFacebookMessenger,
+  faPinterest,
+  faDigg,
+  faTumblr,
+  faReddit,
+  faEvernote
+} from '@fortawesome/free-brands-svg-icons'
+
+// Add the icons to the library for replacing <i class="fa-solid fa-sort"></i> with the intended svg.
+library.add(
+  faSort,
+  faEnvelope,
+  faSearch,
+  faTimes,
+  faFacebook,
+  faXTwitter,
+  faLinkedin,
+  faWhatsapp,
+  faFacebookMessenger,
+  faPinterest,
+  faDigg,
+  faTumblr,
+  faReddit,
+  faEvernote,
+  faCopy,
+  faPrint
+)
