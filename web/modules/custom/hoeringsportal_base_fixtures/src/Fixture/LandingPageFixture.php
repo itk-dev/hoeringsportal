c8ab6a1835f45a96b368419023c312dd6bf3eccf
Jesper Pedersen <pejes@aarhus.dk>
2025-04-11 16:01:31 +0200

    Apply coding standards

--- web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/LandingPageFixture.php
+++ web/modules/custom/hoeringsportal_base_fixtures/src/Fixture/LandingPageFixture.php
@@ -15,8 +15,8 @@ use Drupal\paragraphs\Entity\Paragraph;
  *
  * @package Drupal\hoeringsportal_hearing_fixtures\Fixture
  */
-final class LandingPageFixture extends AbstractFixture implements DependentFixtureInterface, FixtureGroupInterface
-{
+final class LandingPageFixture extends AbstractFixture implements DependentFixtureInterface, FixtureGroupInterface {
+
   /**
    * Constructor.
    */
@@ -25,64 +25,63 @@ final class LandingPageFixture extends AbstractFixture implements DependentFixtu
   /**
    * {@inheritdoc}
    */
-  public function load()
-  {
+  public function load() {
     $page = Node::create([
-      "type" => "landing_page",
-      "title" => "Forside",
-      "field_show_page_title" => false,
+      'type' => 'landing_page',
+      'title' => 'Forside',
+      'field_show_page_title' => FALSE,
     ]);
 
     $pageParagraphs = [];
     $pageParagraphLinks = [];
 
     $paragraphLink = Paragraph::create([
-      "type" => "link",
-      "field_decorative_arrow" => true,
-      "field_icon" => [$this->getReference("media:building-user-solid")],
-      "field_link" => [$this->getReference("node:landing_page:Proposals")],
-      "field_button_variant" => "white",
+      'type' => 'link',
+      'field_decorative_arrow' => TRUE,
+      'field_icon' => [$this->getReference('media:building-user-solid')],
+      'field_link' => [$this->getReference('node:landing_page:Proposals')],
+      'field_button_variant' => 'white',
     ]);
     $paragraphLink->save();
     $pageParagraphLinks[] = $paragraphLink;
 
     $paragraphLink = Paragraph::create([
-      "type" => "link",
-      "field_decorative_arrow" => true,
-      "field_icon" => [$this->getReference("media:comments-solid")],
-      "field_link" => [$this->getReference("node:landing_page:Hearings")],
-      "field_button_variant" => "white",
+      'type' => 'link',
+      'field_decorative_arrow' => TRUE,
+      'field_icon' => [$this->getReference('media:comments-solid')],
+      'field_link' => [$this->getReference('node:landing_page:Hearings')],
+      'field_button_variant' => 'white',
     ]);
     $paragraphLink->save();
     $pageParagraphLinks[] = $paragraphLink;
 
     $paragraphLink = Paragraph::create([
-      "type" => "link",
-      "field_decorative_arrow" => true,
-      "field_icon" => [$this->getReference("media:calendar-days-solid")],
-      "field_link" => [$this->getReference("node:landing_page:Public meetings")],
-      "field_button_variant" => "white",
+      'type' => 'link',
+      'field_decorative_arrow' => TRUE,
+      'field_icon' => [$this->getReference('media:calendar-days-solid')],
+      'field_link' => [$this->getReference('node:landing_page:Public meetings')],
+      'field_button_variant' => 'white',
     ]);
     $paragraphLink->save();
     $pageParagraphLinks[] = $paragraphLink;
 
     $paragraphLink = Paragraph::create([
-      "type" => "link",
-      "field_decorative_arrow" => true,
-      "field_icon" => [$this->getReference("media:folder-open-solid")],
-      "field_link" => [$this->getReference("node:landing_page:Projects")],
-      "field_button_variant" => "white",
+      'type' => 'link',
+      'field_decorative_arrow' => TRUE,
+      'field_icon' => [$this->getReference('media:folder-open-solid')],
+      'field_link' => [$this->getReference('node:landing_page:Projects')],
+      'field_button_variant' => 'white',
     ]);
     $paragraphLink->save();
     $pageParagraphLinks[] = $paragraphLink;
 
     $paragraph = Paragraph::create([
-      "type" => "links_on_a_background_image",
-      "field_paragraph_image" => [$this->getReference("media:Large3")],
-      "field_links_list" => array_map(
+      'type' => 'links_on_a_background_image',
+      'field_paragraph_image' => [$this->getReference('media:Large3')],
+      'field_links_list' => array_map(
         static fn(Paragraph $paragraphLink) => [
-          "target_id" => $paragraphLink->id(),
-          "target_revision_id" => $paragraphLink->getRevisionId(),
+          'target_id' => $paragraphLink->id(),
+          'target_revision_id' => $paragraphLink->getRevisionId(),
         ],
         $pageParagraphLinks
       ),
@@ -91,44 +90,44 @@ final class LandingPageFixture extends AbstractFixture implements DependentFixtu
     $pageParagraphs[] = $paragraph;
 
     $paragraph = Paragraph::create([
-      "type" => "content_list",
-      "field_list_title" => "Igangværende høringer",
-      "field_content_list" => [
-        "target_id" => "latest_hearings",
-        "display_id" => "default",
+      'type' => 'content_list',
+      'field_list_title' => 'Igangværende høringer',
+      'field_content_list' => [
+        'target_id' => 'latest_hearings',
+        'display_id' => 'default',
       ],
     ]);
     $paragraph->save();
     $pageParagraphs[] = $paragraph;
 
     $paragraph = Paragraph::create([
-      "type" => "content_list",
-      "field_list_title" => "Nyeste borgerforslag",
-      "field_content_list" => [
-        "target_id" => "latest_citizen_proposals",
-        "display_id" => "default",
+      'type' => 'content_list',
+      'field_list_title' => 'Nyeste borgerforslag',
+      'field_content_list' => [
+        'target_id' => 'latest_citizen_proposals',
+        'display_id' => 'default',
       ],
     ]);
     $paragraph->save();
     $pageParagraphs[] = $paragraph;
 
     $paragraph = Paragraph::create([
-      "type" => "content_list",
-      "field_list_title" => "Kommende begivenheder",
-      "field_content_list" => [
-        "target_id" => "latest_public_meetings",
-        "display_id" => "default",
+      'type' => 'content_list',
+      'field_list_title' => 'Kommende begivenheder',
+      'field_content_list' => [
+        'target_id' => 'latest_public_meetings',
+        'display_id' => 'default',
       ],
     ]);
     $paragraph->save();
     $pageParagraphs[] = $paragraph;
 
     $paragraph = Paragraph::create([
-      "type" => "content_list",
-      "field_list_title" => "Seneste projekter",
-      "field_content_list" => [
-        "target_id" => "latest_projects",
-        "display_id" => "default",
+      'type' => 'content_list',
+      'field_list_title' => 'Seneste projekter',
+      'field_content_list' => [
+        'target_id' => 'latest_projects',
+        'display_id' => 'default',
       ],
     ]);
     $paragraph->save();
@@ -136,24 +135,24 @@ final class LandingPageFixture extends AbstractFixture implements DependentFixtu
 
     $paragraphButtonLinks = [];
     $paragraphButtonLink = Paragraph::create([
-      "type" => "link",
-      "field_decorative_arrow" => false,
-      "field_link" => [$this->getReference("node:static_page:About")],
-      "field_button_variant" => "petroleum",
+      'type' => 'link',
+      'field_decorative_arrow' => FALSE,
+      'field_link' => [$this->getReference('node:static_page:About')],
+      'field_button_variant' => 'petroleum',
     ]);
     $paragraphButtonLink->save();
     $paragraphButtonLinks[] = $paragraphButtonLink;
 
     $paragraph = Paragraph::create([
-      "type" => "content_promotion",
-      "field_paragraph_image" => [$this->getReference("media:Large3")],
-      "field_lead" => "Om Deltag Aarhus",
-      "field_title" => "Hvad ved du om deltag.aarhus.dk",
-      "field_abstract" => "Se her hvad du kan bruge deltag.aarhus.dk til som borger i Aarhus Kommune",
-      "field_button" => array_map(
+      'type' => 'content_promotion',
+      'field_paragraph_image' => [$this->getReference('media:Large3')],
+      'field_lead' => 'Om Deltag Aarhus',
+      'field_title' => 'Hvad ved du om deltag.aarhus.dk',
+      'field_abstract' => 'Se her hvad du kan bruge deltag.aarhus.dk til som borger i Aarhus Kommune',
+      'field_button' => array_map(
         static fn(Paragraph $paragraphButtonLink) => [
-          "target_id" => $paragraphButtonLink->id(),
-          "target_revision_id" => $paragraphButtonLink->getRevisionId(),
+          'target_id' => $paragraphButtonLink->id(),
+          'target_revision_id' => $paragraphButtonLink->getRevisionId(),
         ],
         $paragraphButtonLinks
       ),
@@ -163,19 +162,19 @@ final class LandingPageFixture extends AbstractFixture implements DependentFixtu
 
     // Add list with static pages.
     $paragraph = Paragraph::create([
-      "type" => "teaser_row",
-      "field_paragraph_title" => "Statiske sider",
-      "field_content" => [$this->getReference("node:static_page:About")],
+      'type' => 'teaser_row',
+      'field_paragraph_title' => 'Statiske sider',
+      'field_content' => [$this->getReference('node:static_page:About')],
     ]);
     $paragraph->save();
     $pageParagraphs[] = $paragraph;
 
     $page->set(
-      "field_section",
+      'field_section',
       array_map(
         static fn(Paragraph $paragraph) => [
-          "target_id" => $paragraph->id(),
-          "target_revision_id" => $paragraph->getRevisionId(),
+          'target_id' => $paragraph->id(),
+          'target_revision_id' => $paragraph->getRevisionId(),
         ],
         $pageParagraphs
       )
@@ -186,30 +185,29 @@ final class LandingPageFixture extends AbstractFixture implements DependentFixtu
     // Set our page as site front page
     // (cf. /admin/site-setup/general > “Pages” > “Front page”)
     // See \Drupal\hoeringsportal_config_settings\Form\ItkGeneralSettingsForm.
-    $this->baseConfig->set("frontpage_id", $page->id());
+    $this->baseConfig->set('frontpage_id', $page->id());
 
     // Set default image for citizen proposal teaser
     // (cf. /admin/site-setup/general > “Default images” > “Citizen proposal default teaser image”)
     // See \Drupal\hoeringsportal_config_settings\Form\ItkGeneralSettingsForm.
     $this->baseConfig->set(
-      "citizen_proposal_teaser_default_image",
-      $this->getReference("media:citizen_proposal_default_image")->id()
+      'citizen_proposal_teaser_default_image',
+      $this->getReference('media:citizen_proposal_default_image')->id()
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getDependencies()
-  {
+  public function getDependencies() {
     return [MediaFixture::class, CitizenProposalLandingPageFixture::class];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getGroups()
-  {
-    return ["node", "landing_page"];
+  public function getGroups() {
+    return ['node', 'landing_page'];
   }
+
 }
